c4ccc3da6c3ab9e84eba81ba2776d4f00cf0c830
Daniel Calderon <danielcalderon@Daniels-MacBook-Pro.local>
2018-02-11 15:40:55 -0800

    Adding week 2 of flix

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Daniel Calderon on 2/5/18.
+//  Copyright © 2018 Daniel Calderon. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
